5bae8892746a69e7a010c77ccf0da070df0ee005
muzzzic546-ui <muzzzic.546@gmail.com>
2025-11-08 15:13:16 -0500

    set up tests!

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# this conftest.py file is intentionally left blank
+
+# it helps pytest understand the structure of the repo 
+
+# to find the app code 
+
+# so that tests can properly import from the app package
