6a6bde73ded95a8e6299d1a7a30633ae3b8c62a5
muzzzic546-ui <muzzzic.546@gmail.com>
2025-11-24 11:37:18 -0500

    Fixed bug + added Gunicorn

--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,10 +1,10 @@
 from pandas import DataFrame
 
-from app.stocks import fetch_stocks_data
+from app.stocks import fetch_stocks_csv
 
 
 def test_fetch_data():
-    df = fetch_stocks_data("NFLX")
+    df = fetch_stocks_csv("NFLX")
     assert isinstance(df, DataFrame)
     assert "adjusted_close" in df.columns
     assert "timestamp" in df.columns
